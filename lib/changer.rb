eac7f8c310553c1e929058503feeff29ce8d1a3e
JordanW <whistler.5@osu.edu>
2021-01-07 16:00:15 -0500

    add: YARD documentation for public methods

--- lib/changer.rb
+++ lib/changer.rb
@@ -3,34 +3,46 @@ class Currency
   attr_accessor :value
   attr_reader :type
   TYPES = [:usd, :eur, :jpy, :gbp, :aud, :cad, :cny, :hkd, :nzd, :chf]
-
+  # Create a new Currency object
+  #
+  #
+  # @param to_type [Symbol]
+  # @param amount [Numeric]
+  # @return [Currency]
   def initialize(to_type, amount)
     raise ArgumentError.new("Invalid type or value") unless verify_valid?(to_type, amount)
 
     @type = to_type
     @value = format_value(amount)
   end
-
+  # Manually set a new value for a Currency object
+  # @param amount [Numeric]
   def value=(amount)
     raise ArgumentError.new("Amount must be a non-negative numeric value") unless amount.is_a?(Numeric) && amount >= 0 
 
     @value = amount.to_f
   end
-
+  # Convert a currency to another type using market rates
+  # @param to_type [Symbol]
+  # @return [Currency] || nil if invalid
   def convert(to_type)
     return nil unless TYPES.include?(to_type) 
 
     new_value = value * Currency.fetch_rate(type, to_type)
     Currency.new(to_type, new_value) 
   end
-
+  # Convert to a different currency at a specified rate
+  # @param to_type [Symbol]
+  # @param rate [Numeric]
+  # @note rate is set based on the calling object as a base
   def convert_at(to_type, rate)
     return nil unless TYPES.include?(to_type)
 
     new_value = value * rate
     Currency.new(to_type, new_value)
   end
-
+  # Convert the calling object to the currency specified by the to_type param
+  # @param to_type [Symbol]
   def convert!(to_type)
     return nil unless TYPES.include?(to_type) 
 
@@ -38,7 +50,9 @@ class Currency
     @type = to_type
     self
   end
-
+  # Convert the calling object to the currency specified by the to_type param
+  # at the rate specified by the rate parameter
+  # @param rate [Numeric]
   def convert_at!(to_type, rate)
     return nil unless TYPES.include?(to_type)
 
@@ -46,11 +60,13 @@ class Currency
     @type = to_type  
     self
   end
-
+  # Return the current value of the Currency object
+  # @return [Numeric]
   def to_f
     value
   end
-  
+  # Return a string formatted as ie; "USD 5.00"
+  # @return [String] 
   def to_s
     format_string
   end
@@ -80,7 +96,9 @@ class Currency
     new_value = (value / other.convert(first_type).value)
     Currency.new(first_type, new_value)
   end
-
+  # Fetch the current exchange rate using the from_type as the base
+  # @param from_type [Symbol]
+  # @param to_type [Symbol]
   def self.fetch_rate(from_type, to_type)
     changer = Changer.new(from_type, to_type)
     changer.rate

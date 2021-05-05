09f40d91851a036894052f6bb2cae51caa803dbb
Jordan Whistler <whistler.5@osu.edu>
2021-05-05 17:40:53 -0400

    New API

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 #### Install 
 Install using `gem install changer`
+Register with Rapidapi.com and set your ENV variable 'RAPID_API_KEY' to your
+X-RapidAPI-Key value.
 
 #### Currency Objects
 Currency objects are created using Currency.new(:sym, amount)
@@ -19,4 +21,3 @@ operation, and return a value in the type of the calling object.  For instance,
 adding US Dollars and Canadian Dollars will return a value in US Dollars, while
 adding Canadian Dollars to US Dollars will return Canadian Dollars.  These 
 methods can be chained ie; USD 3.00 + GBP 2.90 - CAD 6.00 = USD xx.xx
-
--- lib/currency.rb
+++ lib/currency.rb
@@ -9,12 +9,16 @@ class Changer
   def initialize(from_type, to_type)
     from_type = from_type.to_s.upcase
     to_type = to_type.to_s.upcase
-    url = URI("https://api.exchangeratesapi.io/latest?base=#{from_type}")
+    url = URI("https://currency-exchange.p.rapidapi.com/exchange?to=#{to_type}&from=#{from_type}&q=1.0")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
     request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = ENV['RAPID_API_KEY']
+    request["x-rapidapi-host"] = 'currency-exchange.p.rapidapi.com'
+    
     response = http.request(request)
-    @rate = JSON.parse(response.read_body)["rates"]["#{to_type}"].to_f
+    @rate = JSON.parse(response.read_body).to_f
   end
 end
